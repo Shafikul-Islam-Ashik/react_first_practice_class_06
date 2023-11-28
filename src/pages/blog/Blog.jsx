817942257b77f8583f54c754297d2e92a8008d38
Shafikul Islam Molla Ashik <ishafikul720@gmail.com>
2023-11-28 21:24:28 +0600

    practice form data management and useEffect hook

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,10 +1,47 @@
+import { useState } from "react";
 import PageHeader from "../../components/pageHeader/PageHeader";
+import { useEffect } from "react";
 
 const Blog = () => {
+  const [age, setAge] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("I am from Blog Component");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("I am from Count");
+  }, [count]);
+
   return (
     <>
       <PageHeader title="Blog" />
-      <h1>Blog</h1>
+      <div className="container">
+        <br />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <br />
+        <br />
+        <br />
+        <hr />
+        <h1>{count}</h1>
+        <button onClick={() => setCount((prevState) => prevState - 1)}>
+          --
+        </button>
+        <button onClick={() => setCount((prevState) => prevState + 1)}>
+          ++
+        </button>
+        <br />
+        <br />
+        <br />
+      </div>
     </>
   );
 };
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,96 @@
+import { useState } from "react";
 import PageHeader from "../../components/pageHeader/PageHeader";
+import "./Contact.scss";
 
 const Contact = () => {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    cell: "",
+    pass: "",
+    location: "",
+    gender: "",
+  });
+
+  const handleInputValue = (e) => {
+    setInput((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <>
       <PageHeader title="Contact" />
-      <h1>contact</h1>
+      <div className="container contact-container">
+        <h1>Contact</h1>
+        <hr />
+        <div className="user-reg">
+          <form action="">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={input.name}
+              onChange={handleInputValue}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={input.email}
+              onChange={handleInputValue}
+            />
+            <input
+              type="text"
+              placeholder="Cell"
+              name="cell"
+              value={input.cell}
+              onChange={handleInputValue}
+            />
+            <select
+              name="location"
+              value={input.location}
+              onChange={handleInputValue}
+            >
+              <option value="">--Select--</option>
+              <option value="Dhaka">Dhaka</option>
+              <option value="Khulna">Khulna</option>
+              <option value="Barishal">Barishal</option>
+              <option value="Sylhet">Sylhet</option>
+            </select>
+
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Male"
+                  onChange={handleInputValue}
+                />
+                Male
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Female"
+                  onChange={handleInputValue}
+                />
+                Female
+              </label>
+            </div>
+            <input
+              type="text"
+              placeholder="Password"
+              name="pass"
+              value={input.pass}
+              onChange={handleInputValue}
+            />
+            <button>Create Now</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
